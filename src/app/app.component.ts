5560504f43e217b1b084ee6675a82b9b907472fb
WilmerCordoba23 <wilwilmer058@gmail.com>
2023-09-15 14:11:30 -0500

    p2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { WebcamImage } from 'ngx-webcam';
+import { WebcamImage, WebcamInitError, WebcamUtil } from 'ngx-webcam';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  trigger: Subject<void> = new Subject();
+export class AppComponent implements OnInit {
+  trigger: Subject<void> = new Subject<void>();
   previewImage: string = '';
   WebcamImage: any;
   get $trigger(): Observable<void> {
     return this.trigger.asObservable();
   }
 
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.initCamera(); // Solicitar permisos de cámara al iniciar el componente
+  }
+
+  initCamera() {
+    WebcamUtil.getAvailableVideoInputs()
+      .then((mediaDevices: MediaDeviceInfo[]) => {
+        if (mediaDevices && mediaDevices.length > 0) {
+          // Solicitar permisos de cámara solo si hay dispositivos disponibles.
+          this.trigger.next();
+        } else {
+          console.error('No se encontraron dispositivos de cámara disponibles.');
+        }
+      })
+      .catch((error: WebcamInitError) => {
+        console.error('Error al inicializar la cámara: ', error);
+      });
+  }
+
   snapshot(event: WebcamImage) {
-    this.WebcamImage = event
+    this.WebcamImage = event;
     this.previewImage = event.imageAsDataUrl;
   }
 
@@ -25,7 +47,7 @@ export class AppComponent {
   }
 
   proceed() {
-    console.log(this.WebcamImage)
+/*     console.log(this.WebcamImage);
+ */    // Aquí puedes realizar acciones adicionales con la imagen capturada.
   }
-
 }
